644553737d5c1932ec34f33e1ea3648d0676ec82
pier <nmall843r>
2018-12-17 19:10:42 +0100

    updated environments

--- gym_minigrid/envs/__init__.py
+++ gym_minigrid/envs/__init__.py
@@ -16,3 +16,7 @@ from gym_minigrid.envs.obstructedmaze import *
 from gym_minigrid.envs.memory import *
 from gym_minigrid.envs.fourrooms import *
 from gym_minigrid.envs.lavacrossing import *
+
+from gym_minigrid.envs.customs.cleaning import *
+from gym_minigrid.envs.customs.simplecleaning import *
+from gym_minigrid.envs.customs.light import *
